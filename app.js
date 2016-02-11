a7f89db66cbeb8a158562d285bf79b9f8ba699ac
Nabila Diaz <ndiaz@4geeks.com.ve>
2016-02-11 14:53:47 -0430

    Bug fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderComponent from './components/HeaderComponent';
+import HeaderComponent from './components/headerComponent';
 
 React.render(
   <HeaderComponent/>,
